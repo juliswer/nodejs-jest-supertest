5a9237ed4689e877894af6643864ce0dddb85d96
Julian Swerdlin <julianswer36@gmail.com>
2021-12-26 22:43:19 +0100

    new files

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ app.get('/tasks', (req, res) => {
 
 app.post('/tasks', (req, res) => {
     const { title, description } = req.body;
+    if(!title || !description) return res.sendStatus(400);
     res.json({
         title,
         description,
--- test/index.spec.js
+++ test/index.spec.js
@@ -14,23 +14,35 @@ describe('GET /tasks', () => {
 })
 
 describe('POST /tasks', () => {
-    test('should respond with a 200 status code', async () => {
-        const response = await request(app).post('/tasks').send()
-        expect(response.statusCode).toBe(200);
-    });
-    
-    test('should have a content-type: application/json in header', async () => {
-        const response = await request(app).post('/tasks').send()
-        expect(response.header['content-type']).toEqual(
-            expect.stringContaining('json')
-        );
-    });    
 
-    test('should respond with a json object containing the new task with an id', async () => {
-        const response = await request(app).post('/tasks').send({
-            title: 'test',
-            description: 'test',
+    const newTask = {
+        title: 'Test task',
+        description: 'Test description'
+    }
+
+    describe('given a title and description', () => {
+        test('should respond with a 200 status code', async () => {
+            const response = await request(app).post('/tasks').send(newTask);
+            expect(response.statusCode).toBe(200);
+        });
+        
+        test('should have a content-type: application/json in header', async () => {
+            const response = await request(app).post('/tasks').send(newTask)
+            expect(response.header['content-type']).toEqual(
+                expect.stringContaining('json')
+            );
+        });    
+    
+        test('should respond with a json object containing the new task with an id', async () => {
+            const response = await request(app).post('/tasks').send(newTask);
+            expect(response.body.id).toBeDefined();
         })
-        expect(response.body.id).toBeDefined();
+    })
+    
+    describe('when title and description are mising', () => {
+        test('should respond with a 400 status code', async () => {
+            const response = await request(app).post('/tasks').send({});
+            expect(response.statusCode).toBe(400);
+        });
     })
 })
